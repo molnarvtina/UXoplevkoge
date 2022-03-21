a2a7441e254254ae425cd136af4a17d73a9a6568
molnarvtina <95026062+molnarvtina@users.noreply.github.com>
2022-03-21 21:07:33 +0100

    Added review section and toggle button

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
+
+
+let currentReview = 0;
+
 const slider = document.querySelector('.slider');
 
-const leftArrow = document.querySelector('.left');
-const rightArrow = document.querySelector('.right');
+const leftArrow = document.querySelector('.arrow-left');
+const rightArrow = document.querySelector('.arrow-right');
 
 rightArrow.addEventListener('click' , function(){
-slider.style.transform = 'translate(-25%)';
-});
+    
+    currentReview = (currentReview + 1 )%4
+    slider.style.transform =  'translate('+currentReview*-25+'%)';
+
+})
+
+leftArrow.addEventListener('click' , function(){
+    
+    currentReview = (currentReview - 1 + 4 )%4
+    slider.style.transform = 'translate('+currentReview*-25+'%)';
+
+})
+
+const reviewButton = document.querySelectorAll('.reviews-button')[1]
+const reviews = document.querySelector('.reviews')
+
+reviewButton.addEventListener('click' , function(){ 
+    reviews.classList.toggle('not-displayed')
+    
+
+})
